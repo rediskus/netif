9aca0ab27fe4ea3321bfb5544f054a6d4b60332f
root <aeo9457@plantynet.com>
2020-05-04 14:56:24 +0900

    flushadd

--- adapter.go
+++ adapter.go
@@ -34,65 +34,8 @@ type NetworkAdapter struct {
 	Gateway    net.IP
 	AddrSource AddrSource
 	AddrFamily AddrFamily
-}
-
-type valueValidator struct {
-	Type     string
-	Required bool
-	In       []string
-}
-
-var valueValidators = map[string]valueValidator{
-	"hotplug":   {Type: "bool"},
-	"auto":      {Type: "bool"},
-	"name":      {Required: true},
-	"address":   {Type: "IP"},
-	"netmask":   {Type: "IP"},
-	"network":   {Type: "IP"},
-	"broadcast": {Type: "IP"},
-	"gateway":   {Type: "IP"},
-	"addrFam":   {In: []string{"inet", "inet6"}},
-	"source":    {In: []string{"dhcp", "static", "loopback", "manual"}},
-}
-
-func (na *NetworkAdapter) validateAll() error {
-	/*for k, v := range valueValidators {
-		val := nil
-
-	}*/
-	return nil
-}
-
-func (na *NetworkAdapter) validateName() error {
-	return nil
-}
-
-func (na *NetworkAdapter) validateAddress() error {
-	return nil
-}
-
-func (na *NetworkAdapter) validateNetmask() error {
-	return nil
-}
-
-func (na *NetworkAdapter) validateNetwork() error {
-	return nil
-}
-
-func (na *NetworkAdapter) validateBroadcast() error {
-	return nil
-}
-
-func (na *NetworkAdapter) validateGateway() error {
-	return nil
-}
-
-func (na *NetworkAdapter) validateAddrFamily() error {
-	return nil
-}
-
-func (na *NetworkAdapter) validateSource() error {
-	return nil
+	//[added] DNS namserver added to read/write dns-nameservers
+	DNSNS []net.IP
 }
 
 func (na *NetworkAdapter) validateIP(strIP string) (net.IP, error) {
@@ -144,6 +87,16 @@ func (na *NetworkAdapter) SetNetwork(address string) error {
 	return err
 }
 
+//SetDNSNS sets DNS Nameservers
+//[added]
+func (na *NetworkAdapter) SetDNSNS(address string) error {
+	addr, err := na.validateIP(address)
+	if err == nil {
+		na.DNSNS = append(na.DNSNS, addr)
+	}
+	return err
+}
+
 func (na *NetworkAdapter) SetConfigType(configType string) error {
 	switch configType {
 	case "DHCP":
@@ -187,4 +140,16 @@ func (na *NetworkAdapter) ParseAddressFamily(AddressFamily string) (AddrFamily, 
 
 	}
 	return fam, nil
+}
+
+func (na *NetworkAdapter) DNSConcatString() string {
+	message := ""
+	for indx, dns := range na.DNSNS {
+		if indx > 0 || indx < len(na.DNSNS)-1 {
+			message = message + "," + dns.String()
+		} else {
+			message = message + dns.String()
+		}
+	}
+	return message
 }
